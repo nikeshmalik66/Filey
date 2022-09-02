17a755cad404736d8bc16e79fa747d21a4d1543e
OneWeb <nmalik20comp@student.mes.ac.in>
2022-09-03 00:29:24 +0530

    commit changes

---
+++ vfm-admin/_content/users/remember.php
@@ -0,0 +1,5 @@
+<?php
+
+ $_REMEMBER = array (
+  'admin' => 'df05e56ff819fef663527985f991e419',
+);
---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,10 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$jpKGD9sa$wmQ1cU8n3HwRMJiDA7fwP.',
+    'role' => 'superadmin',
+  ),
+);
